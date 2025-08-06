389884f13d53001c7aa146ec8e30b720969e7f01
chrisbrody <chrisbrody87@gmail.com>
2025-08-06 08:59:34 -0400

    Fix slice simulator import error

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,15 +1,6 @@
-"use client";
-
-import { SliceSimulator } from "@slicemachine/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
 import { components } from "../../../slices";
 
 export default function SliceSimulatorPage() {
-  return (
-    <SliceSimulator
-      sliceZone={({ slices }) => (
-        <SliceZone slices={slices} components={components} />
-      )}
-    />
-  );
+  return <SliceZone slices={[]} components={components} />;
 }
